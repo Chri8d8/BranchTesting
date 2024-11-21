e7ce369a884453dc6c72dcfe7a6b415715b015e3
chris <chwe0004@stud.kea.dk>
2024-11-21 13:07:04 +0100

    makeSound commit

--- src/Animal.java
+++ src/Animal.java
@@ -1,4 +1,4 @@
 abstract class Animal {
     private String name;
-
+    abstract void makeSound();
 }
--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,6 @@
 public class Cat extends Animal{
+    @Override
+    void makeSound() {
+
+    }
 }
